787de14a0198f6eab37cb3401775abb61bf7d79e
Caio Bastos <caio.bastos@desbravador.com.br>
2023-04-27 15:04:57 -0300

    crud professor + test

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import router from './app';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(7000, () => {
+  console.log('Server started on http://localhost:7000');
+});
