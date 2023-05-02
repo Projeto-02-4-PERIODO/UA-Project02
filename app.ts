ef12e70ff9c5902abef2896ea192c8af537afc20
Caio Bastos <caio.bastos@desbravador.com.br>
2023-05-02 15:21:34 -0300

    correções app.ts package.json and controller

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import ProfessorController from './src/controller/professorController';
+const router = express.Router();
+
 import dotenv from 'dotenv';
 
 require('dotenv').config();
 dotenv.config();
 
-const router = express.Router();
+
 
 // PROFESSOR
 router.get('/professores', ProfessorController.find);
